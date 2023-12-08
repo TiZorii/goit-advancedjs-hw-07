cc2f1ea9fa8f4ecb7e1388a975f644316f81f0a0
Tetiana Zorii <tanyusha.zoriy@gmail.com>
2023-12-07 23:46:03 -0500

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Welcome home!');
+    } else {
+      console.log('The door is closed. You cannot come in.');
+    }
+  }
+}
+
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is now open.');
+    } else {
+      console.log('Invalid key. The door remains closed.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
